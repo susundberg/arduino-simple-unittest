f8b579cf452456c09cfad963fc9d8b3bd3a712ca
Pauli Salmenrinne <susundberg@gmail.com>
2017-03-23 20:29:34 +0200

    Remove multi unit test from Sconscript and move it to examples as it supposed to be

--- examples/build.sh
+++ examples/build.sh
@@ -27,5 +27,9 @@ cp -a "../tests/" $TARGET_DIR
 cp -a "$SOURCE_DIR/src" $TARGET_DIR
 cp -a "$SOURCE_DIR/tests" $TARGET_DIR
 cd $TARGET_DIR
+PATCH_FILE="../../$SOURCE_DIR.patch"
+if [ -e $PATCH_FILE ]; then
+   patch -p0 < $PATCH_FILE
+fi
 make -f tests/tools/Makefile test_build test_run test_coverage
 
